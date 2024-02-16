e307e5cb5892485e9f2379ae5d2e987ab5dab9da
mdijamco97 <158270909+mdijamco97@users.noreply.github.com>
2024-02-16 08:03:16 -0800

    Linked jQuery and created mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello, My name is Marco Dijamco, Welcome to my Portfolio Site!");
+
+function menuToggle(){
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle'){
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
